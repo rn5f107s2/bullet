4a8b6d6386465f69fbff1144707ca62b8a4fd260
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-17 11:49:20 +0200

    Add OCB buckets

--- src/outputs.rs
+++ src/outputs.rs
@@ -26,3 +26,23 @@ impl<const N: usize> OutputBuckets<ChessBoard> for MaterialCount<N> {
         (pos.occ().count_ones() as u8 - 2) / divisor as u8
     }
 }
+
+#[derive(Clone, Copy, Default)]
+pub struct OCB;
+impl OutputBuckets<ChessBoard> for OCB {
+    const BUCKETS: usize = 2;
+
+    fn bucket(&self, pos: &ChessBoard) -> u8 {
+        let bishop: u8 = 2;
+        
+        let mut iter = pos.into_iter() 
+                          .filter(|(pc, _sq)| (pc & 0b0111) == bishop)
+                          .map(|(pc, sq)| { ( (pc & 0b1000) >> 3, ((sq & 1) ^ ((sq & 8) >> 3))) });
+
+        let Some((pc1, sq1)) = iter.next() else { return 0 };
+        let Some((pc2, sq2)) = iter.next() else { return 0 };
+        let b3 = iter.next();
+        
+        if pc1 != pc2 && sq1 != sq2 && b3 == None { 1 } else { 0 }
+    }
+}
