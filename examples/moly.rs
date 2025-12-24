5142a430406612eb96835993a4604ba0fb11457d
rn5f107s2 <clemens.lerchl@gmail.com>
2025-12-24 21:08:59 +0100

    Fiexd

--- examples/moly.rs
+++ examples/moly.rs
@@ -28,7 +28,7 @@ fn main() {
         .build();
 
     let schedule = TrainingSchedule {
-        net_id: "Chonker".to_string(),
+        net_id: "ChonkerFixed".to_string(),
         eval_scale: SCALE as f32,
         ft_regularisation: 0.0,
         batch_size: 16384,
