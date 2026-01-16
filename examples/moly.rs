582c713f0e7d42aabf456949ad5a5ee9310e0bac
rn5f107s2 <clemens.lerchl@gmail.com>
2026-01-16 17:41:21 +0100

    t

--- examples/moly.rs
+++ examples/moly.rs
@@ -37,7 +37,7 @@ fn main() {
         .build();
 
     let schedule = TrainingSchedule {
-        net_id: "ChonkerFixed".to_string(),
+        net_id: "NormalNetHM".to_string(),
         eval_scale: SCALE as f32,
         ft_regularisation: 0.0,
         batch_size: 16384,
@@ -58,7 +58,7 @@ fn main() {
     };
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 512 };
-    let data_loader = loader::DirectSequentialDataLoader::new(&["data/allshuff.bullet"]);
+    let data_loader = loader::DirectSequentialDataLoader::new(&["data/Moly25kOld.bullet"]);
 
     //trainer.load_from_checkpoint("checkpoints/MolyDataPSQBuckets-200");
     trainer.run(&schedule, &settings, &data_loader);
