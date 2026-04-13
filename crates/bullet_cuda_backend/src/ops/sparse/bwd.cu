6458b92a133cef22e390c4009ba6e2585f48353c
rn5f107s2 <clemens.lerchl@gmail.com>
2026-04-13 11:44:33 +0200

    r

--- crates/bullet_cuda_backend/src/ops/sparse/bwd.cu
+++ crates/bullet_cuda_backend/src/ops/sparse/bwd.cu
@@ -51,7 +51,7 @@ extern "C" __global__ void kernel(
 
     const float tE = op(Y[offset + nRow]) * Yg[offset + nRow];
 
-    if (isnan(tE)) {
+    if (isnan(Y[offset + nRow])) {
         int* f = reinterpret_cast<int*>(0xDEADBEEF);
         *f = 0;
     }
