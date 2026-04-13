8c48bb9539f275e86b50063f27c9e0fa96afc9de
rn5f107s2 <clemens.lerchl@gmail.com>
2026-04-13 13:33:58 +0200

    r

--- crates/bullet_cuda_backend/src/ops/sparse/fwd.rs
+++ crates/bullet_cuda_backend/src/ops/sparse/fwd.rs
@@ -147,6 +147,10 @@ fn fallback_kernel(_bias: Option<bool>) -> String {
             sum += A[j * m + nRow];
         }}
 
-        Y[m * loc + nRow] = op(sum);"
+        Y[m * loc + nRow] = op(sum);
+        
+        if (loc == 0)  {{
+            printf(\"%d\\n\", nRow);
+        }}"
     )
 }
