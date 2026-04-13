dfc29dbdc25f4c3b0792e560061ca5fd83e80007
rn5f107s2 <clemens.lerchl@gmail.com>
2026-04-13 13:47:44 +0200

    r

--- examples/progression/1_simple.rs
+++ examples/progression/1_simple.rs
@@ -34,7 +34,7 @@ fn main() {
         .loss_fn(|output, target| output.sigmoid().squared_error(target))
         .build(|builder, stm_inputs, ntm_inputs| {
             // weights
-            let l0 = builder.new_affine("l0", 768, hl_size);
+            let l0 = builder.new_affine("l0", 768, 2 * hl_size);
             let l1 = builder.new_affine("l1", hl_size, 1);
 
             // inference
