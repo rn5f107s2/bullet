20fe65c5280b6b4e858f81758b8590d177a01b21
rn5f107s2 <clemens.lerchl@gmail.com>
2026-04-13 13:54:04 +0200

    r

--- examples/progression/1_simple.rs
+++ examples/progression/1_simple.rs
@@ -17,7 +17,7 @@ fn main() {
     let hl_size = 12 * 64 * 16;
     let initial_lr = 0.001;
     let final_lr = 0.001 * 0.3f32.powi(5);
-    let superbatches = 1;
+    let superbatches = 100;
     let wdl_proportion = 0.75;
 
     let mut trainer = ValueTrainerBuilder::default()
@@ -48,7 +48,7 @@ fn main() {
         net_id: "1_simple".to_string(),
         eval_scale: 133.0,
         steps: TrainingSteps {
-            batch_size: 1,
+            batch_size: 16384,
             batches_per_superbatch: 1,
             start_superbatch: 1,
             end_superbatch: superbatches,
