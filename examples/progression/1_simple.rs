bb33d149a9e0fac8ce4cfab149c8553e083d2d1d
rn5f107s2 <clemens.lerchl@gmail.com>
2026-04-16 10:39:24 +0200

    dbug

--- examples/progression/1_simple.rs
+++ examples/progression/1_simple.rs
@@ -60,7 +60,7 @@ fn main() {
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
     let dataloader = ViriBinpackLoader::new(
-        "data/moly_oraclegcp_5ks_12khtempmix.vf",
+        "data/unfiltered.vf",
         4096,
         4,
         Filter {
