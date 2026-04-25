376007fcf580c14a626855b3e78efd802a59fd23
rn5f107s2 <clemens.lerchl@gmail.com>
2026-04-25 21:34:22 +0200

    t

--- examples/progression/1_simple.rs
+++ examples/progression/1_simple.rs
@@ -1,6 +1,7 @@
 use bullet_lib::{
     game::{inputs::{Chess768, KernelKnowsBest}, outputs::KingOutputBuckets},
     nn::optimiser::AdamW,
+    nn::optimiser::AdamWParams,
     trainer::{
         save::SavedFormat,
         schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
