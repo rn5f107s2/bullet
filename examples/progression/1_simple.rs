67651e60bd4ca3c8f4e62c714641d929926b7f5f
rn5f107s2 <clemens.lerchl@gmail.com>
2026-05-09 18:34:38 +0200

    t

--- examples/progression/1_simple.rs
+++ examples/progression/1_simple.rs
@@ -49,7 +49,7 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l1b", stricter_clipping);
 
     let schedule = TrainingSchedule {
-        net_id: "QA403_2".to_string(),
+        net_id: "DoubleShuffBuffHalfSkip".to_string(),
         eval_scale: 133.0,
         steps: TrainingSteps {
             batch_size: 16384,
