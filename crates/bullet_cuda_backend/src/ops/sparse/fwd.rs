46d1b589b20397ead9b9d90464a5daae2746ef63
rn5f107s2 <clemens.lerchl@gmail.com>
2026-06-06 11:55:52 +0200

    .

--- crates/bullet_cuda_backend/src/ops/sparse/fwd.rs
+++ crates/bullet_cuda_backend/src/ops/sparse/fwd.rs
@@ -148,7 +148,7 @@ fn fallback_kernel(_bias: Option<bool>) -> String {
 
             if (j <= -1) break;
 
-            printf(\"bpc: %d bsq: %d feat: %d weight_index: %d weight: %3.f out_index: %d \n\", pc, sq, j, j * m * 6 + nRow, A[j * m * 6 + nRow], outRow);
+            printf(\"bpc: %d bsq: %d feat: %d weight_index: %d weight: %3.f out_index: %d \\n\", pc, sq, j, j * m * 6 + nRow, A[j * m * 6 + nRow], outRow);
 
             sum += A[j * m * 6 + nRow];
         }}
