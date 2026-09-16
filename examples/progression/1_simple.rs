671cb4bb086054d7ee38c1e270fcf9a3622f6364
rn5f107s2 <clemens.lerchl@gmail.com>
2026-09-16 21:43:36 +0200

    hmpf

--- examples/progression/1_simple.rs
+++ examples/progression/1_simple.rs
@@ -39,20 +39,21 @@ fn main() {
         .build(|builder, stm_inputs, ntm_inputs| {
             // weights
             let l0 = builder.new_affine("l0", 768 * 6, hl_size);
-            let l1 = builder.new_affine("l1", 2 * hl_size, 16);
-            let l2 = builder.new_affine("l2", 8, 32);
+            let l1 = builder.new_affine("l1", 2 * hl_size, 8);
+            let l2 = builder.new_affine("l2", 16, 32);
             let l3 = builder.new_affine("l3", 32, 1);
 
             l1.init_with_effective_input_size(6 * hl_size);
 
             // inference
             // i dont knwo if or why the slice is necessary
-            let stm_hidden = l0.forward(stm_inputs).crelu().slice_rows(0, hl_size).pairwise_mul();
-            let ntm_hidden = l0.forward(ntm_inputs).crelu().slice_rows(0, hl_size).pairwise_mul();
+            let stm_hidden = l0.forward(stm_inputs).screlu().slice_rows(0, hl_size);
+            let ntm_hidden = l0.forward(ntm_inputs).screlu().slice_rows(0, hl_size);
             let hidden_layer = stm_hidden.concat(ntm_hidden);
 
-            let l1_out = l1.forward(hidden_layer).relu();
-            let l2_out = l2.forward(l1_out).screlu();
+            let l1_out = l1.forward(hidden_layer);
+            let dual   = l1_out.concat(l1_out * l1_out).crelu();
+            let l2_out = l2.forward(dual).screlu();
 
             l3.forward(l2_out)
         });
@@ -63,7 +64,7 @@ fn main() {
 
 
     let schedule = TrainingSchedule {
-        net_id: "PairwiseMaybe".to_string(),
+        net_id: "DefaultDualAct".to_string(),
         eval_scale: 133.0,
         steps: TrainingSteps {
             batch_size: 16384,
